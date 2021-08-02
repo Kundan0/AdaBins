578675c71dc7c9d64897303980aeec18d2ca9bf3
Kundan0 <074bct517.kundan@pcampus.edu.np>
2021-08-02 09:44:55 +0000

    result1.png added

--- run.py
+++ run.py
@@ -17,19 +17,19 @@ N_BINS = 256
 
 # KITTI
 model = UnetAdaptiveBins.build(n_bins=N_BINS, min_val=MIN_DEPTH, max_val=MAX_DEPTH_KITTI)
-pretrained_path = "./pretrained/AdaBins_kitti.pt"
+pretrained_path = "../drive/MyDrive/pretrained/AdaBins_kitti.pt"
 model, _, _ = model_io.load_checkpoint(pretrained_path, model)
 
-test_dataset=ImageFolder("../../formattedvelocity",transform=ToTensor())
+test_dataset=ImageFolder("../drive/MyDrive/formattedvelocity",transform=ToTensor())
 
 image,label=test_dataset[20]
 print(image.size())
 bin_edges, predicted_depth = model(image.unsqueeze(0))
-#print(predicted_depth.size())
+print(predicted_depth.size())
 
-plt.imshow(predicted_depth.squeeze(0).detach().permute(1,2,0),cmap='gray')
+plt.imshow(predicted_depth.squeeze(0).squeeze(0).detach(),cmap='gray')
 #plt.imshow(image.permute(1,2,0),cmap='brg')
-plt.savefig('result.png')
+plt.savefig('result1.png')
 plt.show()
 
 
