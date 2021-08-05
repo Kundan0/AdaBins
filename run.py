572d2b4cd0f495dfc435580306a430aeff3964f2
Kundan0 <074bct517.kundan@pcampus.edu.np>
2021-08-05 05:08:58 +0000

    run.py modified

--- run.py
+++ run.py
@@ -6,8 +6,9 @@ import os
 from torchvision.utils import save_image
 import numpy as np 
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transform
 from torch.utils.data.dataloader import DataLoader
+import matplotlib.pyplot as plt
 MIN_DEPTH = 1e-3
 MAX_DEPTH_NYU = 10
 MAX_DEPTH_KITTI = 80
@@ -15,13 +16,14 @@ MAX_DEPTH_KITTI = 80
 N_BINS = 256 
 
 from torchvision.utils import make_grid
+unloader = transform.ToPILImage()
+loader = transform.Compose([transform.ToTensor()])  
 
-def show_batch(dl):
-    for images, labels in dl:
-        fig, ax = plt.subplots(figsize=(12, 6))
-        ax.set_xticks([]); ax.set_yticks([])
-        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
-        break
+def image_loader(image_name,device):
+    image = Image.open(image_name).convert('RGB')
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
 
 def get_default_device():
     """Pick GPU if available, else CPU"""
@@ -30,47 +32,39 @@ def get_default_device():
     else:
         return torch.device('cpu')
     
-def to_device(data, device):
-    """Move tensor(s) to chosen device"""
-    if isinstance(data, (list,tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
 
-class DeviceDataLoader(DataLoader):
-    """Wrap a dataloader to move data to a device"""
-    def __init__(self, dl, device):
-        self.dl = dl
-        self.device = device
-        
-    def __iter__(self):
-        """Yield a batch of data after moving it to device"""
-        for b in self.dl: 
-            yield to_device(b, self.device)
-
-    def __len__(self):
-        """Number of batches"""
-        return len(self.dl)
 device=get_default_device()
+
 
 model = UnetAdaptiveBins.build(n_bins=N_BINS, min_val=MIN_DEPTH, max_val=MAX_DEPTH_KITTI)
 pretrained_path = "../drive/MyDrive/pretrained/AdaBins_kitti.pt"
 model, _, _ = model_io.load_checkpoint(pretrained_path, model)
-
-test_dataset=ImageFolder("../drive/MyDrive/formattedvelocity",transform=ToTensor())
-test_dl=DataLoader(test_dataset,2,shuffle=False,num_workers=2,pin_memory=True)
-test_dl=DeviceDataLoader(test_dl,device)
 model.to(device)
+dataset_folder='../drive/MyDrive/formattedvelocity/'
 
+save_folder='/content/drive/MyDrive/Depth/'
 
-
-for (images,folder_name) in (test_dl):
-  _,depth=model(images)
+for j in range(1,1075):
+  dataset_folder=dataset_folder+str(j)
+  save_folder=save_folder+str(j)
+  if(not os.path.isdir(save_folder)):
+      print("no save folder ")
+      os.mkdir(save_folder)
+  for i in range(1,41):
+    print("calculating for "+str(i))
+    
+    
+    
+    save_file_name=save_folder+"/"+str(i).zfill(3)+'.png'
+    file_name=dataset_folder+str(i).zfill(3)+'.jpg'
+    image=image_loader(file_name,device)
+    _,depth=model(image)
+    
+    depth=depth.squeeze(0).squeeze(0)
+    plt.imshow(depth.detach().cpu(),cmap='gray')
+    plt.savefig(save_file_name)
+    print('saved '+str(i))
+ 
+    
   
-  for i,image in enumerate(depth):
-   
-    save_image(image,"../drive/MyDrive/Depth/1"+"/"+str(i)+".png")
-  
-
-  
-
 
